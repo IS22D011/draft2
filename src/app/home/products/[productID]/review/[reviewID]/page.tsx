800e15fc051fd89239c1c33b5ce48916726ad158
IS22D011 <is22d011@mandakh.edu.mn>
2024-09-05 09:57:44 +0800

    Update page.tsx

--- src/app/home/products/[productID]/review/[reviewID]/page.tsx
+++ src/app/home/products/[productID]/review/[reviewID]/page.tsx
@@ -1,3 +1,3 @@
 export default function ReviewID({params}:{params:{productID:string; reviewID:string}}){
-    return <h1> id bol {params.productID} review ni {params.reviewID} </h1>;
+    return <h1> baraany id bol {params.productID} harin review ni {params.reviewID} </h1>;
 }
